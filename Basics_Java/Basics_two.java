5381cbe3cc1fcf9a1bb30abed79c6b96303b81a6
archanChamarthi001 <chamarthi.archan@gmail.com>
2024-10-14 22:44:10 +0530

    Add files via upload

--- Basics_Java/Basics_two.java
+++ Basics_Java/Basics_two.java
@@ -5,7 +5,12 @@ public class Basics_two {
         int evenNums;
 
         for(evenNums = 1; evenNums <= 50; evenNums++) {
+
+            if(evenNums % 2 == 0) {
+ 
             System.out.println(evenNums);
+
+            }
         }
     }
 }
