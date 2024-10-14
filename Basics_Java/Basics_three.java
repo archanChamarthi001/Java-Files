2c26e182e5fb19856efe8045a36e8bc4bc9b1650
archanChamarthi001 <chamarthi.archan@gmail.com>
2024-10-15 00:25:58 +0530

    Update Basics_three.java

--- Basics_Java/Basics_three.java
+++ Basics_Java/Basics_three.java
@@ -2,7 +2,7 @@
 
 public class Basics_three {
     public static void main(String[] args) {
-        int num = 15;;
+        int num = 15;
 
         for(int i = 1; i<=10; i++) {
             System.out.printf("%d x %d = %d\n", num, i, num * i);
